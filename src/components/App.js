27147cd42944f4c48edd77259e42ed2c3e7d90b2
Christian Landgren <christian@landgren.nu>
2017-08-20 21:11:39 +0200

    responsive design

--- src/components/App.js
+++ src/components/App.js
@@ -29,40 +29,40 @@ const Sliders = ({parties, editCoalitions}) =>
   </div>
 
 class App extends Component {
-  render () {
-    const {parties, coalitions, groups, history} = this.props
+  sumGroups(parties) {
     const regering = parties.filter(a => a.affiliation === 'regering').sort((a, b) => b.seats - a.seats)
     const opposition = parties.filter(a => a.affiliation === 'opposition').sort((a, b) => b.seats - a.seats)
     const center = parties.filter(a => a.affiliation === 'center').sort((a, b) => b.seats - a.seats)
-    const allParties = regering.concat(center).concat(opposition)
-    const regeringPercentage = Math.round(regering.reduce((t, party) => t + party.seatPercentage, 0) * 1000) / 10
-    const regeringSeats = regering.reduce((t, party) => t + party.seats, 0)
-    const oppositionPercentage = Math.round(opposition.reduce((t, party) => t + party.seatPercentage, 0) * 1000) / 10
-    const oppositionSeats = opposition.reduce((t, party) => t + party.seats, 0)
-    const centerPercentage = Math.round(center.reduce((t, party) => t + party.seatPercentage, 0) * 1000) / 10
-    const centerSeats = center.reduce((t, party) => t + party.seats, 0)
-    const totalPercentage = Math.round(parties.reduce((t, party) => t + party.percentage, 0))
 
-    const legendGroups = [
+    return [
       {
         name: 'regering',
         parties: regering,
         title: `Regering`,
-        subtitle: `${regeringSeats} mandat - ${regeringPercentage}%`
+        seats: regering.reduce((t, party) => t + party.seats, 0),
+        percentage: Math.round(regering.reduce((t, party) => t + party.seatPercentage, 0) * 1000) / 10
       },
       {
         name: 'center',
         parties: center,
         title: `Övriga`,
-        subtitle: `${centerSeats} mandat - ${centerPercentage}%`
+        seats: center.reduce((t, party) => t + party.seats, 0),
+        percentage: Math.round(center.reduce((t, party) => t + party.seatPercentage, 0) * 1000) / 10
       },
       {
         name: 'opposition',
         parties: opposition,
         title: `Opposition`,
-        subtitle: `${oppositionSeats} mandat - ${oppositionPercentage}%`
+        percentage: Math.round(opposition.reduce((t, party) => t + party.seatPercentage, 0) * 1000) / 10,
+        seats: opposition.reduce((t, party) => t + party.seats, 0)
       }
     ]
+  }
+  render () {
+    const {parties, coalitions, groups, history} = this.props
+    const legendGroups = this.sumGroups(parties)
+    const allParties = legendGroups.reduce((a, b) => a.concat(b.parties), [])
+    const totalPercentage = Math.round(parties.reduce((t, party) => t + party.percentage, 0))
 
     return (
       <div className="App">
@@ -74,15 +74,18 @@ class App extends Component {
           <Seating parties={allParties} seatCount={false} />
 
           <div className="LegendContainer">
-            { legendGroups.map(({name, parties, title, subtitle}) =>
+            { legendGroups.map(({name, parties, title, seats, percentage}) =>
               <div className={`LegendGroup${ groups[name].hover ? ' Drop' : '' }`} dragenter={e => e.preventDefault()} ondragover={dragOver} ondrop={dropUpdate(name)} ondragenter={dragEnter(name)} ondragleave={dragLeave(name)}>
                 <h1>{title}</h1>
-                <h2>{subtitle}</h2>
+                <h2 className="wide">{seats} mandat - {percentage}%</h2>
+                <h2 className="small">{percentage}%</h2>
                 {parties.map(({name, seats, seatPercentage, eligable, abbreviation}) =>
-                  <div className={["party", eligable ? "eligable" : "below"].join(' ')} draggable="true" ondragstart={e => e.dataTransfer.setData('Text', abbreviation)}>
+                  <div key={abbreviation} className={["party", eligable ? "eligable" : "below"].join(' ')} draggable="true" ondragstart={e => e.dataTransfer.setData('Text', abbreviation)}>
                     <i className={abbreviation.toLowerCase()}/>
-                    <h1>{name}</h1>
-                    <h2>{seats || "0" } mandat</h2>
+                    <h1 className="wide">{name}</h1>
+                    <h1 className="small">{abbreviation}</h1>
+                    <h2 className="small">{seats || "0" }</h2>
+                    <h2 className="wide">{seats || "0" } mandat</h2>
                   </div>
                 )}
               </div>
