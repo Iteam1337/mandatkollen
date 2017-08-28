d4e9086eb0f287ffed47561ca4387a55de689e38
Rickard Laurin <rickard.laurin@gmail.com>
2017-08-28 13:53:46 +0200

    Sliders styling and overall styling fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,57 @@
 import React from 'pureact'
 import './Footer.css'
 
-const Footer = (props) => (
-   <div className="App-footer">
+const Footer = props =>
+  <div className="App-footer">
+    <div className="App-footer-inner">
       <h2>Om Mandatkollen</h2>
-      <p>Mandatkollen är ett verktyg för att underlätta för medborgare, journalister och analytiker att visualisera och dra slutsatser kring de parlamentariska effekterna av olika hypotetiska valresultat. 
-      Du får gärna använda skärmdumpar från Mandatkollen i egna poster och artiklar så länge du anger källan.</p>
-      <p>Mandatkollen är utvecklad av Iteam och Lennox PR.
-      Vill du använda Mandatkollen som en inbäddad funktion på din hemsida eller nyhetstjänst? Kontakta robert.svensson@lennoxpr.se</p>
+      <p>
+        Mandatkollen är ett verktyg för att underlätta för medborgare,
+        journalister och analytiker att visualisera och dra slutsatser kring de
+        parlamentariska effekterna av olika hypotetiska valresultat. Du får
+        gärna använda skärmdumpar från Mandatkollen i egna poster och artiklar
+        så länge du anger källan.
+      </p>
+      <p>
+        Mandatkollen är utvecklad av Iteam och Lennox PR. Vill du använda
+        Mandatkollen som en inbäddad funktion på din hemsida eller nyhetstjänst?
+        Kontakta robert.svensson@lennoxpr.se
+      </p>
 
       <h3>Så funkar det:</h3>
       <p>
-      I visualiseringen fördelas mandaten över Riksdagens 349 platser utifrån ett hypotetiskt valresultat som du bestämmer. Om du flyttar ett reglage för ett parti så anpassas alla de andra partiernas andelar proportionerligt. 
-      Om ett parti hamnar under riksdagsspärren på 4 procent så tilldelas de inga mandat.
-      Fördelningen av mandat är justerade enligt 2018 års regler för mandatfördelning.
-      Har du förslag på hur vi kan förbättra den här tjänsten? Kontakta christian.landgren@iteam.se.</p>
+        I visualiseringen fördelas mandaten över Riksdagens 349 platser utifrån
+        ett hypotetiskt valresultat som du bestämmer. Om du flyttar ett reglage
+        för ett parti så anpassas alla de andra partiernas andelar
+        proportionerligt. Om ett parti hamnar under riksdagsspärren på 4 procent
+        så tilldelas de inga mandat. Fördelningen av mandat är justerade enligt
+        2018 års regler för mandatfördelning. Har du förslag på hur vi kan
+        förbättra den här tjänsten? Kontakta christian.landgren@iteam.se.
+      </p>
 
       <h3>Underlag</h3>
-      <p>Röstresultatet som visas initialt kommer från SCB <a href="http://www.scb.se/hitta-statistik/statistik-efter-amne/demokrati/partisympatier/partisympatiundersokningen-psu/pong/statistiknyhet/partisympatier-maj-2017/">valundersökning som genomfördes i maj 2017.</a></p>
+      <p>
+        Röstresultatet som visas initialt kommer från SCB{' '}
+        <a href="http://www.scb.se/hitta-statistik/statistik-efter-amne/demokrati/partisympatier/partisympatiundersokningen-psu/pong/statistiknyhet/partisympatier-maj-2017/">
+          valundersökning som genomfördes i maj 2017.
+        </a>
+      </p>
 
-      <a href="https://iteam.se"><img alt="Iteam logo" className="logo" src="https://iteam.se/content/images/iteam_white.png"/></a>
-      <a href="http://lennoxpr.se"><img alt="Lennox PR logo" className="logo" src="http://lennoxpr.se/wp-content/uploads/2017/04/logo-lennox.png"/></a>
+      <a href="https://iteam.se">
+        <img
+          alt="Iteam logo"
+          className="logo"
+          src="https://iteam.se/content/images/iteam_white.png"
+        />
+      </a>
+      <a href="http://lennoxpr.se">
+        <img
+          alt="Lennox PR logo"
+          className="logo"
+          src="http://lennoxpr.se/wp-content/uploads/2017/04/logo-lennox.png"
+        />
+      </a>
     </div>
-)
+  </div>
 
 export default Footer
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,17 +1,14 @@
 import React from 'pureact'
 import './Slider.css'
 
-export default ({party, onchange}) => (
-  <div>
-    <input 
-      className={party.abbreviation.toLowerCase()} 
-      key={party.abbreviation} 
-      type="range" 
-      oninput={e => onchange(parseInt(e.target.value, 10) )} value={party.percentage } 
-      step="1" 
-      min="0" 
-      max="35" />
-    <div className="bar"></div>
-  </div>
-)
-    
+export default ({ party, onchange }) =>
+  <input
+    className={`slider-${party.abbreviation.toLowerCase()}`}
+    key={party.abbreviation}
+    type="range"
+    oninput={e => onchange(parseInt(e.target.value, 10))}
+    value={party.percentage}
+    step="1"
+    min="0"
+    max="35"
+  />
