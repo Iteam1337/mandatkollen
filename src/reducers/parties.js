8a50376ae9e70cc59cb3389e6e3a90cfca537ed3
Christian Landgren <christian@landgren.nu>
2017-09-06 23:43:48 +0200

    Use same colors in graph as in legend

--- src/reducers/parties.js
+++ src/reducers/parties.js
@@ -2,16 +2,16 @@ import { Parliament } from '../lib/parliament'
 import polls from '../lib/history'
 
 const initialState = [
-  {id: 1, name: 'Kristdemokraterna', percentage: 3.10, affiliation: 'opposition', colour: '#366da3', abbreviation: 'KD'},
+  {id: 1, name: 'Kristdemokraterna', percentage: 3.10, affiliation: 'opposition', colour: '#3163A6', abbreviation: 'KD'},
   {id: 2, name: 'Moderaterna', percentage: 21.00, affiliation: 'opposition', colour: '#2F80ED', abbreviation: 'M'},
-  {id: 3, name: 'Liberalerna', percentage: 5.70, affiliation: 'opposition', colour: '#378cab', abbreviation: 'L'},
-  {id: 4, name: 'Centerpartiet', percentage: 11.00, affiliation: 'opposition', colour: '#78ae5a', abbreviation: 'C'},
-  {id: 5, name: 'Sverigedemokraterna', percentage: 13.50, affiliation: 'center', colour: '#e7d960', abbreviation: 'SD'},
-  {id: 6, name: 'Socialdemokraterna', percentage: 31.40, affiliation: 'regering', colour: '#c13b38', abbreviation: 'S'},
-  {id: 7, name: 'Miljöpartiet', percentage: 5.00, affiliation: 'regering', colour: '#acc768', abbreviation: 'MP'},
-  {id: 8, name: 'Vänsterpartiet', percentage: 7.10, affiliation: 'center', colour: '#992522', abbreviation: 'V'},
-  {id: 9, name: 'FI', percentage: 2.40, affiliation: 'center', colour: '#fab', abbreviation: 'FI'},
-  {id: 10, name: 'Övriga', affiliation: 'center', colour: '#000', percentage: 2.20, abbreviation: 'Ö'}
+  {id: 3, name: 'Liberalerna', percentage: 5.70, affiliation: 'opposition', colour: '#56CCF2', abbreviation: 'L'},
+  {id: 4, name: 'Centerpartiet', percentage: 11.00, affiliation: 'opposition', colour: '#27AE60', abbreviation: 'C'},
+  {id: 5, name: 'Sverigedemokraterna', percentage: 13.50, affiliation: 'center', colour: '#F2C94C', abbreviation: 'SD'},
+  {id: 6, name: 'Socialdemokraterna', percentage: 31.40, affiliation: 'regering', colour: '#E04B49', abbreviation: 'S'},
+  {id: 7, name: 'Miljöpartiet', percentage: 5.00, affiliation: 'regering', colour: '#219653', abbreviation: 'MP'},
+  {id: 8, name: 'Vänsterpartiet', percentage: 7.10, affiliation: 'center', colour: '#B12827', abbreviation: 'V'},
+  {id: 9, name: 'FI', percentage: 2.40, affiliation: 'center', colour: '#D96B97', abbreviation: 'FI'},
+  {id: 10, name: 'Övriga', affiliation: 'center', colour: '#4D4D4D', percentage: 2.20, abbreviation: 'Ö'}
 ]
 
 let parliament = new Parliament(initialState)
