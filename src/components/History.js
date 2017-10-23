95efdd1dbb66bf6b48ffc903b22382275ae76883
Christian Landgren <christian@landgren.nu>
2017-10-23 16:22:12 +0200

    bugfix typography

--- src/components/History.js
+++ src/components/History.js
@@ -7,7 +7,7 @@ const chooseBaseVotes = (votes) => store.dispatch({type: 'CHOOSE_BASE_VOTES', vo
 const History = ({history}) => {
   return (<div className="App-history">
     <img src="https://pollofpolls.se/poll_img/pollofpolls.svg"/>
-    <input type="range" className="polls" max={history.length-1} oninput={event => chooseBaseVotes(history[history.length - 1 - event.target.value])} />
+    <input type="range" className="polls" max={history.length-1} value="100" oninput={event => chooseBaseVotes(history[history.length - 1 - event.target.value])} />
     <small>{history.date}</small>
   </div>)
 }
--- src/reducers/parties.js
+++ src/reducers/parties.js
@@ -16,8 +16,7 @@ const parties = [
 
 let parliament = new Parliament(parties)
 let initialState = polls.fetchHistory().then(polls => {
-  parliament.updatePolls(polls[0].parties)
-  return parliament.seats
+  return new Parliament(parliament.updatePolls(polls[0].parties)).seats
 })
 
       
