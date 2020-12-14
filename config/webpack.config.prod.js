d1c0f9d158c3759fd9cbafc136d29ccf97e6a0bf
Christian Landgren <christian@landgren.nu>
2020-12-14 17:26:38 +0100

    added font preload

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -13,6 +13,8 @@ const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const paths = require('./paths');
 const getClientEnvironment = require('./env');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const FontPreloadPlugin = require('webpack-font-preload-plugin');
+ 
  
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
@@ -101,6 +103,7 @@ module.exports = {
       // please link the files into your node_modules/ and let module-resolution kick in.
       // Make sure your source files are compiled, as they will not be processed in any way.
       new ModuleScopePlugin(paths.appSrc),
+      new FontPreloadPlugin()],
     ],
   },
   module: {
